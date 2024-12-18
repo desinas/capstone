123245101112235b5085db4b91687d8ed26a8d86
Dimitrios Kalkasinas <disinas@outlook.com>
2024-12-18 18:36:57 +0100

    Update ec2_autoscaling.tf

--- ec2_autoscaling.tf
+++ ec2_autoscaling.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-061dd8b45bc7deb3d"
   instance_type = "t2.micro"
-  key_name      = "my-key-pair"
+  key_name      = "vockey"
   vpc_security_group_ids = [aws_security_group.web.id]
 
   user_data = <<-EOF
