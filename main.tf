270d4b48c5a0485471ab63e2aa6f0d821783a003
Dimitrios Kalkasinas <disinas@outlook.com>
2024-12-18 19:34:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
