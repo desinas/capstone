7edd5b09baf284166b152bc42744d3cb29e6c8e9
Dimitrios Kalkasinas <disinas@outlook.com>
2024-12-18 18:58:14 +0100

    Update aws_vpc_subn_igateway.tf

--- aws_vpc_subn_igateway.tf
+++ aws_vpc_subn_igateway.tf
@@ -56,3 +56,22 @@ resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_instance" "web" {
+  ami           = "ami-061dd8b45bc7deb3d"
+  instance_type = "t2.micro"
+  key_name      = "vockey"
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              service httpd start
+              chkconfig httpd on
+            EOF
+
+  tags = {
+    Name = "web-server"
+  }
+}
