9404351ea99c5813de302204118162c4d018526b
Dimitrios Kalkasinas <disinas@outlook.com>
2024-12-18 19:09:11 +0100

    Update aws_vpc_subn_igateway.tf

--- aws_vpc_subn_igateway.tf
+++ aws_vpc_subn_igateway.tf
@@ -61,7 +61,7 @@ resource "aws_instance" "web" {
   ami           = "ami-061dd8b45bc7deb3d"
   instance_type = "t2.micro"
   key_name      = "vockey"
-  vpc_security_group_ids = [aws_security_group.web.id]
+  vpc_security_group_ids = [sg-0f39022e023ac7430] //aws_security_group.web.id
 
   user_data = <<-EOF
               #!/bin/bash
